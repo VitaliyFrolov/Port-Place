c46ff08a28c29652c4d19f12a2244197439f2c49
Anzor <magometov_a.z@mail.ru>
2024-04-23 17:58:21 +0300

    feat: Начал писать модели

--- server/apps/core/models.py
+++ server/apps/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class GamingTables(models.Model):
+    class Meta:
+        verbose_name = "Стол"
+        verbose_name_plural = "Столы"
+
+    TYPE_OF_GAME = {"pin": "pinpong", "bil": "billiard"}
+
+    game = models.CharField(verbose_name="Вид игры")
