1f94936cc58dd6ef84b2ac9df9d6ba2777957bb2
Vitaliy <vitaliy.frolov.dev@gmail.com>
2024-05-24 21:02:38 +0300

    feat: add useScrollTrigger hook

--- client/src/shared/ui/Header/ui/Header.tsx
+++ client/src/shared/ui/Header/ui/Header.tsx
@@ -1,28 +1,34 @@
+'use client'
+import { useScrollTrigger } from '@/shared/hooks/useScrollTrigger';
 import { Container } from '../../Container';
 import { Logo } from '../../Logo';
 import styles from './Header.module.scss';
 
 export const Header = () => {
-    return (
-        <div className={styles.header__wrapper}>
-            <Container>
-                <header className={styles.header}>
-                <Logo />
-                <nav className={styles.navigation}>
-                    <ul className={styles.navigation__list}>
-                        <li>
-                            О нас
-                        </li>
-                        <li>
-                            Резиденты
-                        </li>
-                        <li>
-                            Игровая зона
-                        </li>
-                    </ul>
-                </nav>
-                </header>
-            </Container>
-        </div>
-    );
+    const scrolled = useScrollTrigger(0);
+
+    if (scrolled) {
+        return (
+            <div className={styles.header__wrapper}>
+                <Container>
+                    <header className={styles.header}>
+                    <Logo />
+                    <nav className={styles.navigation}>
+                        <ul className={styles.navigation__list}>
+                            <li>
+                                О нас
+                            </li>
+                            <li>
+                                Резиденты
+                            </li>
+                            <li>
+                                Игровая зона
+                            </li>
+                        </ul>
+                    </nav>
+                    </header>
+                </Container>
+            </div>
+        );
+    }
 };
